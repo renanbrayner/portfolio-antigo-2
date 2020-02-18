b14377ae4361fb688bdbaacdcb9eaad37d71e8e3
Renan Brayner <renanbrayner@gmail.com>
2020-02-18 19:37:01 -0300

    Updates

---
+++ precache-manifest.c3a7e857909c4b7d9284bf81d8300e98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5c467b0ae30b1a325524f393041251",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47e721a1fd3a3a59017d",
+    "url": "/portfolio/static/css/main.d40010c4.chunk.css"
+  },
+  {
+    "revision": "ed933b53e2ecdb0db4bb",
+    "url": "/portfolio/static/js/2.edc3081b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.edc3081b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e721a1fd3a3a59017d",
+    "url": "/portfolio/static/js/main.e0887bc1.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "46726c13ea25d7e3823e2bd69a821759",
+    "url": "/portfolio/static/media/gitsearch.46726c13.png"
+  },
+  {
+    "revision": "07a22ba694b228ec5cd17b82c4265ef5",
+    "url": "/portfolio/static/media/pedra-papel-tesoura.07a22ba6.png"
+  },
+  {
+    "revision": "4c79e87560f818e46c7282c195929a14",
+    "url": "/portfolio/static/media/profile.4c79e875.jpg"
+  }
+]);
